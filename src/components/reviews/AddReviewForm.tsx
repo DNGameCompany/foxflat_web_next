922d97087ff58bfbc9c83bd384370f703aaef8fa
admin <aydogdievdmitriy@gmail.com>
2025-10-11 13:35:57 +0300

    fix | reviews

--- src/components/reviews/AddReviewForm.tsx
+++ src/components/reviews/AddReviewForm.tsx
@@ -64,7 +64,7 @@ export default function AddReviewForm({ onNewReview }: AddReviewFormProps) {
             <div className="space-y-4">
                 <div>
                     <label htmlFor="name" className="block text-xs font-medium text-neutral-400 uppercase tracking-wide mb-2">
-                        Ім'я
+                        Ім&#39;я
                     </label>
                     <input
                         id="name"
